f0b1e19ce2d674793198de92fae8c354ecb65b38
bachelor98 <2596445436@qq.com>
2022-04-12 21:10:55 +0800

    init

--- src/main.js
+++ src/main.js
@@ -4,23 +4,41 @@ const x = localStorage.getItem('x')
 const xObject = JSON.parse(x)    //把字符串重新变成对象
 
 const hashMap = xObject || [
-    { logo:'./image/acfun.jpeg',logoType:'image',url:'https://www.acfun.cn'},
-    { logo:'B',logoType:'text',url:'https://www.bilibili.com/'},
-    { logo:'G',logoType:'text',url:'https://github.com/'},
+    { logo:'A',logoType:'image',url:'https://www.acfun.cn'},
+    { logo:'B',logoType:'text',url:'https://www.bilibili.com'},
+    { logo:'G',logoType:'text',url:'https://github.com'},
 ]
+
+const simplifyUrl = (url)=>{
+    return url.replace('https://','')
+    .replace('http://','')
+    .replace('www.','')
+    .replace(/\/.*/,'')
+}
 
 const render = ()=>{
     $siteList.find('li:not(.last)').remove()     //唯独不要最后一个last
-    hashMap.forEach(node=>{
+    hashMap.forEach((node,index)=>{
         const $li = $(`
         <li>
-            <a href="${node.url}">
             <div class="site">
-            <div class="logo">${node.logo[0]}</div>
-            <div class="link">${node.url}</div>
+            <div class="logo">${node.logo}</div>
+            <div class="link">${simplifyUrl(node.url)}</div>
+            <div class="close">
+                <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-close"></use>
+                </svg>
             </div>
-            </a>
+            </div>
         </li>`).insertBefore($lastLi)
+        $li.on('click',()=>{
+            window.open(node.url)
+        })
+        $li.on('click','.close',(e)=>{
+            e.stopPropagation()     //阻止冒泡（不是冒泡排序）
+            hashMap.splice(index,1)
+            render()
+        })
     })
 }
 
@@ -31,7 +49,7 @@ $('.addButton').on('click',()=>{
     if(url.indexOf('http')!==0){    //判断是否以http开头
         url = 'https://' + url
     }
-    hashMap.push({logo:url[0],logoType:'text',url:url});
+    hashMap.push({logo:simplifyUrl(url)[0].toUpperCase(),logoType:'text',url:url});
     render()
 });
 
@@ -39,3 +57,4 @@ window.onbeforeunload = ()=>{
     const String = JSON.stringify(hashMap)  //这可以把一个对象变成字符串
     window.localStorage.setItem('x',String)
 }
+
